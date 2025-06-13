baa466241638ee219142bbe1046bdbc4cd92ea60
paolo9688 <paolo.pz1@gmail.com>
2025-06-13 12:00:44 +0200

    created post method

--- src/main/java/com/example/crud_test_exercise/controller/UserController.java
+++ src/main/java/com/example/crud_test_exercise/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.example.crud_test_exercise.controller;
 
+import com.example.crud_test_exercise.entity.User;
 import com.example.crud_test_exercise.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +15,10 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    // crea un nuovo utente:
+    @PostMapping("/create-user")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        return ResponseEntity.ok(userService.createUser(user));
+    }
 }
--- src/main/java/com/example/crud_test_exercise/service/UserService.java
+++ src/main/java/com/example/crud_test_exercise/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.crud_test_exercise.service;
 
+import com.example.crud_test_exercise.entity.User;
 import com.example.crud_test_exercise.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,9 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    // crea un nuovo utente:
+    public User createUser(User newUser){
+        return userRepository.save(newUser);
+    }
 }
