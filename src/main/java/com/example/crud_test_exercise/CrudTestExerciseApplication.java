cc196b358537b1e32ff0c116ee6f6a3382102424
paolo9688 <paolo.pz1@gmail.com>
2025-06-13 10:22:01 +0200

    first commit

---
+++ src/main/java/com/example/crud_test_exercise/CrudTestExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud_test_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudTestExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudTestExerciseApplication.class, args);
+	}
+
+}
