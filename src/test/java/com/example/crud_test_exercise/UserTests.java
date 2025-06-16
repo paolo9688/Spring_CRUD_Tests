d1b349d248f268c12e82c68c4c4db4816ecc490b
paolo9688 <paolo.pz1@gmail.com>
2025-06-16 11:56:20 +0200

    created crud tests for create user method and find users method

---
+++ src/test/java/com/example/crud_test_exercise/UserTests.java
@@ -0,0 +1,85 @@
+package com.example.crud_test_exercise;
+
+import com.example.crud_test_exercise.controller.UserController;
+import com.example.crud_test_exercise.entity.User;
+import com.example.crud_test_exercise.service.UserService;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@ActiveProfiles(value = "dev")
+@AutoConfigureMockMvc
+class UserTests {
+
+	@MockitoBean
+	private UserService userService;
+
+	@Autowired
+	private UserController userController;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	private User user;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		user = new User("Mario", "Rossi", true);
+		user.setId(1L);
+
+		mockMvc.perform(post("/api/user")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(user)));
+	}
+
+	@Test
+	void contextLoads() {
+		assertThat(userController).isNotNull();
+	}
+
+	@Test
+	public void testCreate() throws Exception{
+		when(userService.createUser(any(User.class))).thenReturn(user);
+
+		mockMvc.perform(post("/api/user/create-user")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(user)))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.name").value(user.getName()))
+				.andDo(print());
+	}
+
+	@Test
+	public void testGet() throws Exception{
+		when(userService.getUsers()).thenReturn(List.of(user));
+
+		mockMvc.perform(get("/api/user/find-users")
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andDo(print());
+	}
+}
